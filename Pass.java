0797e6d4ca6d868f8f5464de626b1c782ff87f1a
Wojciech Chojnacki <quak@tlen.pl>
2011-04-13 23:07:36 +0200

    new pass paths added

--- Pass.java
+++ Pass.java
@@ -23,25 +23,148 @@ public class Pass
 		Motor.C.regulateSpeed(false);
 		
 		robot.pilot.setSpeed(800);
-		//robot.pilot.setRotateSpeed(1000);
 		robot.run();
     }
 	
 	public void run () throws Exception
 	{
-		pilot.rotate(70);
-		pilot.steer(-47);
-		Thread.sleep(2500);
-		pilot.stop();
+		int delay = 0;
+		int action = 0;
+		int button = 0;
 		
-		/*
-		pilot.travel(90, true);
-		while (pilot.isMoving()) ;
-		pilot.stop();
-		pilot.travel(-10);
-		*/
+		//ask for delay
+		while (button != 1)
+		{
+			LCD.clear();
+			
+			if (button == 4) delay++;
+			if (button == 2) delay--;
+			
+			if (delay < 0) delay = 0;
+			
+			LCD.drawString("Delay", 1, 1);
+			LCD.drawInt(delay, 1, 2);
+			
+			button = Button.waitForPress();
+		}
+
+		
+		//ask for action
+		button = 0;
+		while (button != 1)
+		{
+			LCD.clear();
+			
+			if (button == 4) action++;
+			if (button == 2) action--;
+			
+			if (action < 0) action = 0;
+			if (action > 5) action = 5;
+			
+			LCD.drawString("Action", 1, 1);
+			LCD.drawString(mapAction(action), 1, 2);
+			
+			
+			button = Button.waitForPress();
+		}
+		
+		
+		//delay action
+		Thread.sleep(delay * 1000);
+
+		
+		if (action == 0) steerTight(false);
+		else if (action == 1) steerWide(false);
+		else if (action == 2) steerTight(true);
+		else if (action == 3) steerWide(true);
+		else if (action == 4) edge(false);
+		else if (action == 5) edge(true);
 		
 	}
+	
+	
+	public String mapAction(int action)
+	{
+		if (action == 0) return "right tight";
+		if (action == 1) return "right wide";
+		if (action == 2) return "left tight";
+		if (action == 3) return "left wide";
+		if (action == 4) return "right edge";
+		if (action == 5) return "left edge";
+		
+		return "undefined";
+	}
+	
+	
+	
+	public void steerTight(boolean reverse) throws Exception
+	{
+		int rotate = 70;
+		int steer = -46;
+		
+		if (reverse)
+		{
+			rotate = -rotate;
+			steer = -steer;
+		}
+		
+		pilot.rotate(rotate);
+		pilot.steer(steer);
+		Thread.sleep(2300);
+		pilot.stop();
+	}
+	
+	
+	public void steerWide(boolean reverse) throws Exception
+	{
+		
+		int rotate = 110;
+		int arcrad = -30;
+		int arctrav = -105;
+		
+		if (reverse)
+		{
+			rotate = -rotate;
+			arcrad = -arcrad;
+			arctrav = -arctrav;
+		}
+		
+		pilot.rotate(rotate);
+		pilot.travelArc(arcrad, arctrav);
+		pilot.travel(65);
+	}
+	
+	public void edge(boolean reverse) throws Exception
+	{
+		int rotate = 180;
+		int arc1 = -25;
+		int trav1 = -50;
+		int arc2 = -40;
+		int trav2 = -70;
+		int arc3 = -35;
+		int trav3 = -60;
+		
+		if (reverse)
+		{
+			rotate = -rotate;
+			arc1 = -arc1;
+			trav1 = -trav1;
+			arc2 = -arc2;
+			trav2 = -trav2;
+			arc3 = -arc3;
+			trav3 = -trav3;
+			
+		}
+		
+		pilot.rotate(rotate);
+		pilot.travelArc(arc1, trav1);
+		pilot.travelArc(arc2, trav2);
+		pilot.travelArc(arc3, trav3);
+		pilot.travel(10);
+	}
+	
 }
+
+
 
 
